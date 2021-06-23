d23d8cc3a49a5c5e9af90c75eea01800584026ab
rohmatjulianto <juliantojuly@gmail.com>
2021-06-23 09:04:05 +0700

    problem 3

--- src/com/joule/Problem3.java
+++ src/com/joule/Problem3.java
@@ -1,7 +1,33 @@
 package com.joule;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Problem3 {
     public static void main(String[] args) {
+        Integer[] arr = new Integer[]{1,3,5};
+        System.out.println(solution(arr));
 
+    }
+
+    public static int solution(Integer[] arr){
+        int solution = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] > 0 ){
+                set.add(arr[i]);
+            }else{
+                solution = 1;
+            }
+        }
+
+        if (solution != 1){
+            for (int i = 0; i < arr.length+1; i++){
+                if (!set.contains(i)){
+                    solution = i;
+                }
+            }
+        }
+        return solution;
     }
 }
